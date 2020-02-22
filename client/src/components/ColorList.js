bf2f3b035f92270d1ed3dd325b1f1abca105c8b1
Josh schaben <jschaben42@gmail.com>
2020-02-21 20:42:18 -0600

    setting up routes

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -18,6 +19,17 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit )
+    .then(res => {
+      console.log(res)
+      console.log(colorToEdit)
+      updateColors([...colors.filter(color => color.id !== colorToEdit.id), res.data]);
+      setEditing(false);
+    })
+    .catch(err => {
+      console.log('edit failed', err)
+    })
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
@@ -25,6 +37,15 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/colors/${color.id}`)
+      .then(res => {
+        console.log(res)
+        updateColors(colors.filter(color => color.id !== res.data))
+      })
+      .catch(err => {
+        console.log('could not delete' ,err)
+      })
   };
 
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+  const login = e => {
+    e.preventDefault();
+    console.log(credentials);
+    axiosWithAuth().post('/login', credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/protected')
+      })
+      .catch(err => console.log(err))
+  };
+
+  const handleChange = event => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value
+    });
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+      <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={credentials.password}
+            onChange={handleChange}
+          />
+          <button>Log in</button>
+      </form>
     </>
   );
 };
